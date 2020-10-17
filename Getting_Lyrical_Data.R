2175ede547662c7623ffb6db0aae994562270631
jakepscott <jakepscott16@gmail.com>
2020-10-17 18:59:02 -0400

    Made script to get a tidy data frame where each row is a word and the song+album it came from

---
+++ Getting_Lyrical_Data.R
@@ -0,0 +1,35 @@
+# Loading Libraries and data ------------------------------------------------------------
+library(tidyverse)
+library(tidytext)
+
+RTJ_Songs <- read_rds("Data/RTJ_Songs.rds")
+
+
+# Tokenizing the lyrics ---------------------------------------------------
+#Right now the lyrics are stored in a list column, with each entry for each song being a set of 
+#strings, approximately one string per line in a song. I need to get it so that each entry in the 
+#lyric column in just one line for that song
+
+#First I make an empty tibble for binding
+RTJ_lyrics <- tibble(lyrics=character(0),
+                     song=character(0),
+                     album=character(0))
+
+#Here I take a given entry, unlist it, turn it into a tibble, and pair that tibble of lyrics lines 
+#with the name of the song it came from. This results in a tibble where each line from each song 
+#is paired with the song (and album) it came from
+for (i in 1:nrow(RTJ_Songs)) {
+  print(i)
+  to_bind <- RTJ_Songs$Lyrics[[i]] %>% tibble(lyrics=.) %>% mutate(song=RTJ_Songs$name[i],
+                                                                   album=RTJ_Songs$album[i])
+  RTJ_lyrics <- RTJ_lyrics %>% rbind(to_bind)
+}
+
+#Reordering the columns purely for readability reasons
+RTJ_lyrics <- RTJ_lyrics %>% select(album,song,lyrics)
+
+#Unnesting tokens (so each row is a word and the album+song it came from)
+RTJ_lyrics <- RTJ_lyrics %>% unnest_tokens(word,lyrics)
+
+#Saving
+saveRDS(RTJ_lyrics,"Data/RTJ_lyrics.rds")
