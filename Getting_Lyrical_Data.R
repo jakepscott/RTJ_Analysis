bcc12f841c4b63db679e2f547012192f3f07946a
jakepscott <jakepscott16@gmail.com>
2020-10-18 15:21:23 -0400

    Keeping track number in lyrical data

--- Getting_Lyrical_Data.R
+++ Getting_Lyrical_Data.R
@@ -13,7 +13,8 @@ RTJ_Songs <- read_rds("Data/RTJ_Songs.rds")
 #First I make an empty tibble for binding
 RTJ_lyrics <- tibble(lyrics=character(0),
                      song=character(0),
-                     album=character(0))
+                     album=character(0),
+                     track_number=integer(0))
 
 #Here I take a given entry, unlist it, turn it into a tibble, and pair that tibble of lyrics lines 
 #with the name of the song it came from. This results in a tibble where each line from each song 
@@ -21,12 +22,13 @@ RTJ_lyrics <- tibble(lyrics=character(0),
 for (i in 1:nrow(RTJ_Songs)) {
   print(i)
   to_bind <- RTJ_Songs$Lyrics[[i]] %>% tibble(lyrics=.) %>% mutate(song=RTJ_Songs$name[i],
-                                                                   album=RTJ_Songs$album[i])
+                                                                   album=RTJ_Songs$album[i],
+                                                                   track_number=RTJ_Songs$track_number[i])
   RTJ_lyrics <- RTJ_lyrics %>% rbind(to_bind)
 }
 
 #Reordering the columns purely for readability reasons
-RTJ_lyrics <- RTJ_lyrics %>% select(album,song,lyrics)
+RTJ_lyrics <- RTJ_lyrics %>% select(album,song,lyrics,track_number)
 
 #Unnesting tokens (so each row is a word and the album+song it came from)
 RTJ_lyrics <- RTJ_lyrics %>% unnest_tokens(word,lyrics)
