10296f3bf638342c097de8ea6793aaa0859447de
jakepscott <jakepscott16@gmail.com>
2020-10-18 19:30:52 -0400

    Preliminary tf-idf

--- Initial_Analysis.R
+++ Initial_Analysis.R
@@ -129,7 +129,18 @@ RTJ_lyrics %>%
 
 
 # tf-idf ------------------------------------------------------------------
+tf_idf <- RTJ_lyrics %>% count(album,word_clean) %>% bind_tf_idf(term = word_clean,document = album,n = n)
 
+tf_idf %>% 
+  group_by(album) %>% 
+  top_n(10, tf_idf) %>% 
+  mutate(word_clean=reorder_within(x=word_clean,by = tf_idf,within = album)) %>% 
+  ggplot(aes(x=word_clean,y=tf_idf,fill=album)) +
+  geom_col() +
+  facet_wrap(~album,scales = "free") +
+  coord_flip() +
+  scale_x_reordered() +
+  scale_y_continuous(expand=c(0,0))
 
 
 
