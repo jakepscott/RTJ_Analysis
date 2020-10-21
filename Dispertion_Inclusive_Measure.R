62546fe5f2e15d9d4afc18b830ce50a45582efce
jakepscott <jakepscott16@gmail.com>
2020-10-21 00:34:37 -0400

    Cleaned up, thoroughly commented, and made self-contained the script for the dispersion-corrected measure

--- Dispertion_Inclusive_Measure.R
+++ Dispertion_Inclusive_Measure.R
@@ -1,51 +1,67 @@
+# Loading Libraries and Data ----------------------------------------------
+library(tidyverse)
+library(readr)
+windowsFonts(`Roboto Condensed`=windowsFont("Roboto Condensed"))
+RTJ_lyrics <- read_rds("Data/RTJ_lyrics.rds")
+data("stop_words")
+#Removing stop words
+RTJ_lyrics <- RTJ_lyrics %>% anti_join(stop_words)
+
+#Setting up empty tible. This will contain data on each album-word combination, including the 
+#the proportion of words outside that album that are a given word, and a scaling factor, which measures
+#what proportion of songs outside the given album does the given word appear in
 outside_values <- tibble(album=character(0),
                          word=character(0),
                          prop_outside=double(0),
                          outside_scaling=double(0))
-for (i in 1:4) {
-  #For each album...
-  print(paste("i is",i))
-  album_names <- c("Run the Jewels", "RTJ 2", "RTJ 3", "RTJ 4")
-  album_to_analyze <- album_names[i]
-  for (z in 1:nrow(RTJ_lyrics %>% filter(album==album_to_analyze) %>% distinct(word))) {
-    #For each word in the given album
-    print(paste("z is",z))
-    words <- RTJ_lyrics %>% filter(album==album_to_analyze) %>%  distinct(word) %>% pull(word)
-    word_of_interest <- words[z]
-    
+for (i in c("Run the Jewels", "RTJ 2", "RTJ 3", "RTJ 4")) {
+  print(i)
+  for (z in RTJ_lyrics %>% filter(album==i) %>% distinct(word) %>% pull(word)) {
     #Get the number of words outside the album
-    total_outside_words <- RTJ_lyrics %>% filter(album!=album_to_analyze) %>% distinct(word) %>% nrow()
+    total_outside_words <- RTJ_lyrics %>% 
+      filter(album!=i) %>% 
+      distinct(word) %>% 
+      nrow()
     #Get the number of times the given word appears outside the given album
-    total_word_of_interest_outside <- RTJ_lyrics %>% filter(album!=album_to_analyze,
-                                                            word==word_of_interest) %>% nrow()
-    
-    
+    total_word_of_interest_outside <- RTJ_lyrics %>% 
+      filter(album!=i, word==z) %>% 
+      nrow()
     #Make the proportion
     prop_outside_to_paste <- (total_word_of_interest_outside/total_outside_words)
     
-    #Get the percent of songs outside the album of interest that the given word is in
-    num_outside_songs <- RTJ_lyrics %>% filter(album!=album_to_analyze) %>% distinct(song) %>% nrow() #num of outside songs
-    songs_word_is_in <- RTJ_lyrics %>% #Get number of songs word is in
-      filter(album!=album_to_analyze, 
-             word==word_of_interest) %>% distinct(song) %>% nrow()
-    outside_scaling_to_paste <- songs_word_is_in/num_outside_songs #make scaling measure
+    #Get the number of songs outside the given album
+    num_outside_songs <- RTJ_lyrics %>% filter(album!=i) %>% distinct(song) %>% nrow() #num of outside songs
+    #Get the number of songs outside the given album that the given word appears in
+    songs_word_is_in <- RTJ_lyrics %>%
+      filter(album!=i, 
+             word==z) %>%
+      distinct(song) %>% 
+      nrow()
+    #Make into proportion (which I use as the scaling factor)
+    outside_scaling_to_paste <- songs_word_is_in/num_outside_songs 
+    
     #Make into a tibble
-    to_bind <- tibble(album=album_to_analyze,
-                      word=word_of_interest,
+    to_bind <- tibble(album=i,
+                      word=z,
                       prop_outside=prop_outside_to_paste,
                       outside_scaling=outside_scaling_to_paste)
+    
     #Bind onto a big tibble which will have each album-word pair and the corresponding prop_outside column
     outside_values <- outside_values %>% rbind(to_bind)
   }
 }
+
+#Multiply the proportion of words outside the given album made up by the given word by the 
+#proption of songs outside the given album the given word is in (dispersion_corrected_measure)
 outside_values <- outside_values %>% mutate(scaled_prop_outside=prop_outside*outside_scaling)
 
-Relative_Importance <- RTJ_lyrics %>% 
-  select(album,word,word_clean) %>% 
+#Combine this importance measure with the lyrics tibble
+Disp_Relative_Importance <- RTJ_lyrics %>% 
+  select(album,song,word,word_clean) %>% 
   left_join(outside_values,by=c("album","word"))
 
 #Making album back into an ordered factor
-Relative_Importance$album <- factor(Relative_Importance$album,
+Disp_Relative_Importance$album <- factor(Disp_Relative_Importance$album,
                                     levels=c("Run the Jewels",
                                              "RTJ 2",
                                              "RTJ 3",
@@ -55,33 +71,47 @@ Relative_Importance$album <- factor(Relative_Importance$album,
                                              "RTJ 3",
                                              "RTJ 4"))
 
-Relative_Importance <- Relative_Importance %>% 
-  #Getting a song column back
-  cbind(RTJ_lyrics$song) %>% 
-  as_tibble() %>% 
-  rename("song"=`RTJ_lyrics$song`) %>% 
-  #Getting percent_inside
-  group_by(album,word) %>% 
+#Get percent of words in given album made up by given word
+Disp_Relative_Importance <- Disp_Relative_Importance %>% 
+  #Get number of times given word appears in given album 
+  group_by(album,word) %>%
   mutate(n=n()) %>% 
   ungroup() %>% 
+  #Get number of words in that album
   group_by(album) %>% 
   mutate(total_words=n()) %>% 
   ungroup() %>%
-  mutate(prop_inside=(n/total_words)) %>% 
-  #Getting proportion of songs a given word is in
+  #Divide number of times given word appears by total words to get proportion
+  mutate(prop_inside=(n/total_words)) 
+
+#Get proportion of songs in a given album that a given word appears in
+Disp_Relative_Importance <- Disp_Relative_Importance %>% 
   group_by(album,word) %>% 
+  #Get number of songs given word appears in
   mutate(song_appearances=length(unique(song))) %>% 
   ungroup() %>% 
   group_by(album) %>% 
+  #get number of songs in given album
   mutate(total_songs=length(unique(song))) %>% 
   ungroup() %>% 
+  #Divide number of songs given word appears in by total number of songs in that album
   mutate(prop_songs_in=(song_appearances/total_songs)) %>% 
   #Multipling proportion of words in an album made up by word x by the proportion of songs word x is in
   mutate(scaled_prop_inside=prop_inside*prop_songs_in) 
 
-test <- Relative_Importance %>% mutate(difference=scaled_prop_inside-scaled_prop_outside)
-test_clean <- test %>% distinct(album,word,.keep_all = T)
-test_clean %>% 
+#Getting difference between scaled inside proportion and scaled outside proportion, which is the scaled
+#importance measure
+Disp_Relative_Importance <- Disp_Relative_Importance %>% mutate(difference=scaled_prop_inside-scaled_prop_outside)
+
+#Saving so I don't have to run the loop each time
+saveRDS(Disp_Relative_Importance,"Data/Disp_Relative_Importance.rds")
+
+#Getting just distinct entries (only need one row for each album-word pair now since I am done counting)
+Disp_Relative_Importance <- read_rds("Data/Disp_Relative_Importance.rds")
+Disp_Relative_Importance_Clean <- Disp_Relative_Importance %>% distinct(album,word,.keep_all = T)
+
+#Making a quick and dirty plot
+Disp_Relative_Importance_Clean %>% 
   group_by(album) %>% 
   top_n(10,difference) %>% 
   ungroup() %>% 
