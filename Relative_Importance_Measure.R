f1af7147b5a1bb2b8c699add2b16a4052546fd97
jakepscott <jakepscott16@gmail.com>
2020-10-21 00:41:24 -0400

    Improved speed of for loop by correcting the indexes. Next step will be memory pre-allocation

--- Relative_Importance_Measure.R
+++ Relative_Importance_Measure.R
@@ -11,30 +11,29 @@ RTJ_lyrics <- RTJ_lyrics %>% anti_join(stop_words)
 #of all words made up by that word outside the given album. So if the album is RTJ2 and the word
 #is run, the percent_outside column will be the proportion of words outside RTJ2 that are "run"
 outside_values <- tibble(album=character(0),word=character(0),percent_outside=double(0))
-for (i in 1:4) {
-  #For each album...
-  print(paste("i is",i))
-  album_names <- c("Run the Jewels", "RTJ 2", "RTJ 3", "RTJ 4")
-  album_to_analyze <- album_names[i]
-  for (z in 1:nrow(RTJ_lyrics %>% filter(album==album_to_analyze) %>% distinct(word))) {
-    #For each word in the given album
-    print(paste("z is",z))
-    words <- RTJ_lyrics %>% filter(album==album_to_analyze) %>%  distinct(word) %>% pull(word)
-    word_of_interest <- words[z]
-    
+tictoc::tic()
+for (i in c("Run the Jewels", "RTJ 2", "RTJ 3", "RTJ 4")) {
+  print(i)
+  for (z in RTJ_lyrics %>% filter(album==i) %>% distinct(word) %>% pull(word)) {
     #Get the number of words outside the album
-    total_outside_words <- RTJ_lyrics %>% filter(album!=album_to_analyze) %>% distinct(word) %>% nrow()
+    total_outside_words <- RTJ_lyrics %>% 
+      filter(album!=i) %>% 
+      distinct(word) %>% 
+      nrow()
     #Get the number of times the given word appears outside the given album
-    total_word_of_interest_outside <- RTJ_lyrics %>% filter(album!=album_to_analyze,
-                                                      word==word_of_interest) %>% nrow()
+    total_word_of_interest_outside <- RTJ_lyrics %>% 
+      filter(album!=i,
+             word==z) %>% 
+      nrow()
     #Make the proportion
     percent_outside_to_paste <- (total_word_of_interest_outside/total_outside_words)*100
     #Make into a tibble
-    to_bind <- tibble(album=album_to_analyze,word=word_of_interest,percent_outside=percent_outside_to_paste)
+    to_bind <- tibble(album=i,word=z,percent_outside=percent_outside_to_paste)
     #Bind onto a big tibble which will have each album-word pair and the corresponding percent_outside column
     outside_values <- outside_values %>% rbind(to_bind)
   }
 }
+tictoc::toc()
 #Join the outside_values tibble to the RTJ_lyrics tibble, so for each word with will now have the value
 #for the proportion of words outside the given album are made up by that given word. So if the album is
 #RTJ4 and the word is week, the percent_outside column, which is .029, means that week makes up .029% of words in 
@@ -154,12 +153,5 @@ smooth_top_10 %>%
         panel.grid = element_blank(),
         strip.text = element_text(face="bold",colour = "white",size=rel(1.2)),
         plot.background = element_rect(fill="grey20"))
-
-
-# Relative Importance and Dispersion --------------------------------------
-RTJ_lyrics %>% 
-  filter(word=="kill") %>% 
-  group_by(album) %>% 
-  mutate(test=length(unique(song))) %>% distinct(album,.keep_all = T)
 
   
